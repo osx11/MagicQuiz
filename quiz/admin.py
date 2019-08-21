f34f4f12ad6173241f85f6285e4ad37de749b93a
osx11 <osx11@ya.ru>
2019-08-22 00:21:33 +0600

    show id of fields in admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -4,16 +4,16 @@ from .models import Quiz, QuizQuestions, QuestionAnswers
 
 
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'id')
 
 
 class QuizQuestionsAdmin(admin.ModelAdmin):
-    list_display = ('question', 'quiz_id')
+    list_display = ('question', 'quiz_id', 'id')
     list_filter = ('quiz__name',)
 
 
 class QuestionAnswersAdmin(admin.ModelAdmin):
-    list_display = ('answer', 'quiz_questions_id')
+    list_display = ('answer', 'quiz_questions_id', 'id')
     list_filter = ('quiz_questions__quiz__name', 'quiz_questions__question')
 
 
